52f5b4d973318ac585fcecbb9cfe3f399c59300f
anurag-thummar <anuragthummar98@gmail.com>
2025-01-22 13:02:17 +0530

    Updates

--- assets/js/pre-loader.js
+++ assets/js/pre-loader.js
@@ -15,19 +15,17 @@
 
 
 window.addEventListener("load", function () {
-  console.log("Page fully loaded, hiding loader...");
-  // document.getElementById("loader");
-  // Show content with animation (Choose either 'slide-right' or 'slide-left')
+
   const content = document.getElementById("loaderSlide");
   content.style.display = "block";
-
+  
   // Enable page scrolling
   document.body.style.overflow = "auto";
 
   // Hide the loaderSlide after a 1-second delay
   setTimeout(function() {
     document.getElementById("loaderSlide").style.display = "none";
-  }, 2000); // 1000 milliseconds = 1 second
+  }, 2000); 
 });
 
 
